7173272ff9777c19d5d257654550cf92c8b676dd
ryanschuttig <ryanschuttig@gmail.com>
2023-12-21 23:48:52 -0500

    added github links in footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 
 function App() {
-  return <Header />;
-  return <Footer />;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <p className="footer">
+      <a href="https://github.com/EpicGibbon" target="_blank">
+        EpicGibbon
+      </a>
+      <a href="https://github.com/ryanschuttig" target="_blank">
+        ryanschuttig
+      </a>
+    </p>
+  );
+}
+
+export default Footer;
