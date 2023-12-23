a7be1f27f944fa07804c91a85da3b282bd985d24
ryanschuttig <ryanschuttig@gmail.com>
2023-12-23 00:47:12 -0500

    created filter, needs fixing

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Filter.scss";
+
+function Filter() {
+  return (
+    <div>
+      <input type="text" placeholder="Filter..." className="filter" />
+    </div>
+  );
+}
+
+export default Filter;
--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -3,6 +3,7 @@ import ProjectCard from "../../components/project-card/ProjectCard";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import "./MainPage.scss";
+import Filter from "../../components/filter/Filter";
 
 export const MainPage = () => {
   const [project, setProject] = useState([
@@ -18,11 +19,24 @@ export const MainPage = () => {
       name: "To-Do List",
       href: "/href",
     },
+    {
+      name: "Simon Says",
+      href: "/href",
+    },
+    {
+      name: "Password Generator",
+      href: "/href",
+    },
+    {
+      name: "Recipe List",
+      href: "/href",
+    },
   ]);
 
   return (
     <>
       <Header />
+      <Filter />
       <div className="container">
         {project.length
           ? project.map((project) => (
