7dc72438d18e1822a9faedad79aca7c35efb0150
Anthony Aguiar <michant1997@gmail.com>
2024-01-11 15:19:54 -0500

    added id's to data and added key prop to projectCard children

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -8,26 +8,32 @@ import Filter from "../../components/filter/Filter";
 export const MainPage = () => {
   const [project, setProject] = useState([
     {
+      id: 1,
       name: "Weather Dashboard",
       href: "/weather",
     },
     {
+      id: 2,
       name: "Calculator",
       href: "/href",
     },
     {
+      id: 3,
       name: "To-Do List",
       href: "/href",
     },
     {
+      id: 4,
       name: "Simon Says",
       href: "/href",
     },
     {
+      id: 5,
       name: "Password Generator",
       href: "/href",
     },
     {
+      id: 6,
       name: "Recipe List",
       href: "/href",
     },
@@ -40,7 +46,7 @@ export const MainPage = () => {
       <div className="container">
         {project.length
           ? project.map((project) => (
-              <ProjectCard name={project.name} href={project.href} />
+              <ProjectCard name={project.name} key={project.id} href={project.href} />
             ))
           : ""}
       </div>
